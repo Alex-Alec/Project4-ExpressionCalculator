572d8d8a83bc425ead7bb4f02847c49eb8dc3aa7
Alexander Sun <technoal9@gmail.com>
2020-12-08 20:17:34 -0500

    worked through -literal bugs

--- src/SimpleExpressionParser.java
+++ src/SimpleExpressionParser.java
@@ -96,18 +96,8 @@ public class SimpleExpressionParser implements ExpressionParser {
 					return new DoubleChildExpression (parseStartExpression(str.substring(0, i)),
 							parseStartExpression(str.substring(i+1)), str.charAt(i));
 
-				} else if(str.charAt(i) == '-'){
-					if(i - 1 == 0){
-						continue;
-					} else if(i+1 < str.length()){
-						if(str.charAt(i+1) == '.'){
-							continue;
-						}
-						try {
-							parseLiteralExpression(str.substring(i, i+2));
-						} catch(ExpressionParseException e){ // not a literal
-							throw new ExpressionParseException("Invalid String Exception");
-						}
+				} else if(str.charAt(i) == '-'){ // Check if the operator matches an subtractive expression
+					if(isNegativeLiteral(str, i)){
 						continue;
 					}
 					return new DoubleChildExpression (parseStartExpression(str.substring(0, i)),
@@ -118,6 +108,38 @@ public class SimpleExpressionParser implements ExpressionParser {
 
 		// If it isn't an additive expression, try the multiplicative expression
 		return parseMultiplicativeExpression(str);
+	}
+
+	/**
+	 * Checks if the negative sign leads to a negative literal or not
+	 * @param str string working in
+	 * @param i position of negative sign
+	 * @return if the negative sign in str at position i is a negative literal
+	 * @throws ExpressionParseException
+	 */
+	protected boolean isNegativeLiteral(String str, int i) throws ExpressionParseException {
+
+		// If negative sign is at the very end, throw an exception
+		if(i + 1 >= str.length()){
+			throw new ExpressionParseException("Invalid String Exception");
+		}
+
+		// If the negative sign isn't followed by a literal or a ., then there is an invalid string
+		if(!(str.charAt(i+1) == '.' || isLiteralExpression(str.substring(i, i+2)))){
+			throw new ExpressionParseException("Invalid String Exception");
+		}
+
+		// If negative sign is at the very beginning, it's a negative literal, not an expression
+		if(i == 0){
+			return true;
+		}
+
+		// If the the negative sign is preceded by an x or a literal, then it is part of a subtractive expression, not a negative literal
+		if (isLiteralExpression(Character.toString(str.charAt(i-1))) || str.charAt(i-1) == 'x'){
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 	/**
@@ -233,53 +255,61 @@ public class SimpleExpressionParser implements ExpressionParser {
 	}
 
 	/**
+	 * Helper method to check if it's a literal expression
+	 * @param str
+	 * @return if it's a literal expression
+	 */
+	protected boolean isLiteralExpression(String str){
+		// From https://stackoverflow.com/questions/3543729/how-to-check-that-a-string-is-parseable-to-a-double/22936891:
+		final String Digits     = "(\\p{Digit}+)";
+		final String HexDigits  = "(\\p{XDigit}+)";
+		// an exponent is 'e' or 'E' followed by an optionally
+		// signed decimal integer.
+		final String Exp        = "[eE][+-]?"+Digits;
+		final String fpRegex    =
+				("[\\x00-\\x20]*"+ // Optional leading "whitespace"
+						"[+-]?(" +         // Optional sign character
+						"NaN|" +           // "NaN" string
+						"Infinity|" +      // "Infinity" string
+
+						// A decimal floating-point string representing a finite positive
+						// number without a leading sign has at most five basic pieces:
+						// Digits . Digits ExponentPart FloatTypeSuffix
+						//
+						// Since this method allows integer-only strings as input
+						// in addition to strings of floating-point literals, the
+						// two sub-patterns below are simplifications of the grammar
+						// productions from the Java Language Specification, 2nd
+						// edition, section 3.10.2.
+
+						// Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
+						"((("+Digits+"(\\.)?("+Digits+"?)("+Exp+")?)|"+
+
+						// . Digits ExponentPart_opt FloatTypeSuffix_opt
+						"(\\.("+Digits+")("+Exp+")?)|"+
+
+						// Hexadecimal strings
+						"((" +
+						// 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
+						"(0[xX]" + HexDigits + "(\\.)?)|" +
+
+						// 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
+						"(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" +
+
+						")[pP][+-]?" + Digits + "))" +
+						"[fFdD]?))" +
+						"[\\x00-\\x20]*");// Optional trailing "whitespace"
+		return str.matches(fpRegex);
+	}
+
+	/**
 	 * Parse Literal Expression
 	 * @param str
 	 * @return
 	 * @throws ExpressionParseException
 	 */
 	protected Expression parseLiteralExpression (String str) throws ExpressionParseException {
-		// From https://stackoverflow.com/questions/3543729/how-to-check-that-a-string-is-parseable-to-a-double/22936891:
-		final String Digits     = "(\\p{Digit}+)";
-		final String HexDigits  = "(\\p{XDigit}+)";
-		// an exponent is 'e' or 'E' followed by an optionally 
-		// signed decimal integer.
-		final String Exp        = "[eE][+-]?"+Digits;
-		final String fpRegex    =
-		    ("[\\x00-\\x20]*"+ // Optional leading "whitespace"
-		    "[+-]?(" +         // Optional sign character
-		    "NaN|" +           // "NaN" string
-		    "Infinity|" +      // "Infinity" string
-
-		    // A decimal floating-point string representing a finite positive
-		    // number without a leading sign has at most five basic pieces:
-		    // Digits . Digits ExponentPart FloatTypeSuffix
-		    // 
-		    // Since this method allows integer-only strings as input
-		    // in addition to strings of floating-point literals, the
-		    // two sub-patterns below are simplifications of the grammar
-		    // productions from the Java Language Specification, 2nd 
-		    // edition, section 3.10.2.
-
-		    // Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
-		    "((("+Digits+"(\\.)?("+Digits+"?)("+Exp+")?)|"+
-
-		    // . Digits ExponentPart_opt FloatTypeSuffix_opt
-		    "(\\.("+Digits+")("+Exp+")?)|"+
-
-		    // Hexadecimal strings
-		    "((" +
-		    // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
-		    "(0[xX]" + HexDigits + "(\\.)?)|" +
-
-		    // 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
-		    "(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" +
-
-		    ")[pP][+-]?" + Digits + "))" +
-		    "[fFdD]?))" +
-		    "[\\x00-\\x20]*");// Optional trailing "whitespace"
-
-		if (str.matches(fpRegex)) {
+		if (isLiteralExpression(str)) {
 			return new NoChildExpression(str);
 		}
 		throw new ExpressionParseException("Invalid String Exception");
